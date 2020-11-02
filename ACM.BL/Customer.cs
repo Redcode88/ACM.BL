9205f6ab5d66eab1d7b723f0721d1575954542de
medomohsen293 <medomohsen293@gmail.com>
2020-11-02 03:15:15 +0200

    OOPRepo

---
+++ ACM.BL/Customer.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+    public class Customer
+    {
+        public int CustomerId { get; private set; }
+        public string EmailAddres { get; set; }
+        public string FirstName { get; set; }
+        private string _Lastname;
+        public string FullName
+        {
+
+            get
+            {
+                string fullName = Lastname;
+                if (!string.IsNullOrEmpty(FirstName))
+                {
+                    if(!string.IsNullOrWhiteSpace(fullName))
+
+                    {
+                        fullName += ", ";
+                    }
+
+                    fullName += FirstName;
+                }
+               return fullName; 
+            }
+        }
+        public string Lastname
+        {
+            get
+            {
+                return _Lastname;
+            }
+            set
+            {
+                _Lastname = value;
+            }
+        }
+        // Validation Of Customer Data ....
+        public bool Validate()
+        {
+            var isValid = true;
+            if (string.IsNullOrWhiteSpace(Lastname)) isValid = false;
+            if (string.IsNullOrWhiteSpace(EmailAddres)) isValid = false;
+            return isValid ;
+        }
+
+        public static int InstanseCount { get; set; }
+       
+    }
+
+   
+  
+        
+}
---
+++ TEST/ACM.BLTEST/CustomerTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ACM.BL;
+
+namespace ACM.BLTEST
+{
+    [TestClass]
+    public class CustomerTest
+    {
+        [TestMethod]
+        public void FullNameTestValid()
+
+        {
+            //-- Arrang with object initializer Way
+            Customer customer = new Customer
+            {
+                FirstName="medo",
+                Lastname="mohsen"
+            };
+            //Actuale
+            string Expected = "medo, mohsen";
+            string Actual = customer.FullName;
+            // Assert
+            Assert.AreEqual(Expected, Actual);
+        }
+        
+        [TestMethod]
+        public void FullNameFirstNameEmpty()
+        {
+            //-- Arrang with object initializer Way
+            Customer customer = new Customer
+            {
+                Lastname = "mohsen"
+            };
+            //Actuale
+            string Expected = "mohsen";
+            string Actual = customer.FullName;
+            // Assert
+            Assert.AreEqual(Expected, Actual);
+        }
+        [TestMethod]
+        public void StaticTest()
+        {
+            var c1 = new Customer();
+            c1.FirstName = "Ahmed";
+            Customer.InstanseCount += 1;
+
+            var c2 = new Customer();
+            c2.FirstName = "Mohamed";
+            Customer.InstanseCount += 1;
+
+            var c3 = new Customer();
+            c3.FirstName = "Mohsen";
+            Customer.InstanseCount += 1;
+
+            Assert.AreEqual(3, Customer.InstanseCount);
+        }
+    }
+}
